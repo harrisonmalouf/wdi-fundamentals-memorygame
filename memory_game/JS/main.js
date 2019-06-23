9c52f0ef93fe4dc4bc86a554ca8555bf4457420b
Harrison Malouf <harrisonmalouf@gmail.com>
2019-06-23 16:36:12 +1000

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,7 +1,23 @@
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+// console.log("User flipped" + cardOne);
+// console.log("User flipped" + cardTwo);
 
-var cardOne "king";
-var cardTwo "king":
-var cardThree "queen"
-var cardFour "queen"
+var cards = ["queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(cardOne)
+console.log(cardTwo)
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("you've found a match!")
+	} else {
+		alert("Try again!")
+	}
+}
